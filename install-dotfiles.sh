ce17bd860d6466f27ab9e4e06713a11a9da20791
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2024-09-22 18:48:55 +0200

    Remove zsh installation from install-dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -12,9 +12,3 @@ if [ $? = 0 ]; then
 fi
 config checkout
 config config status.showUntrackedFiles no
-
-# ZSH
-zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
-sudo apt install fzf -y
-zsh -c "git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search"
