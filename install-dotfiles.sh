e8de9825c2d44e9d7898ca63018d23ee8a31a08e
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2024-09-22 18:42:38 +0200

    Update install-dotfiles.sh

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -16,6 +16,6 @@ config config status.showUntrackedFiles no
 # ZSH
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo apt install fzf -y
+bash sudo apt install fzf -y
 git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search
-source ~/.zshrc
+bash source ~/.zshrc
