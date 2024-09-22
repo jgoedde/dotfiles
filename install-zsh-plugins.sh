a4fa1abe4021010cd0c99abcc1ddda3d3fbf1799
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2024-09-22 18:49:25 +0200

    Create install-zsh-plugins.sh

---
+++ install-zsh-plugins.sh
@@ -0,0 +1,4 @@
+zsh -c "git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+zsh -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+sudo apt install fzf -y
+zsh -c "git clone https://github.com/joshskidmore/zsh-fzf-history-search ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-fzf-history-search"
