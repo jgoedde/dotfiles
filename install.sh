3354168d9fbea17bb8d23d60bdc7f5afc7d6f064
Julian Gödde <juliangd02@gmail.com>
2026-06-22 12:53:49 +0200

    add thunar with kitty default action

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 DOTFILES="$HOME/dotfiles"
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
 
-stow -t ~ -d "$DOTFILES" zsh albert inshellisense fastfetch kitty
+stow -t ~ -d "$DOTFILES" zsh albert inshellisense fastfetch kitty Thunar
 
 # Third-Party-Plugins
 while read -r repo; do
