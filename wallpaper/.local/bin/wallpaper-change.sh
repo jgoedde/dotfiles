a5a8e2f92911671f6c5753ada4524f6ddb438639
Julian Gödde <j.goedde@pragmaticminds.de>
2026-06-25 08:33:27 +0200

    feat(matugen): add Obsidian Material You theme template
    Vault path is machine-specific so can't be tracked. Template renders
    to ~/.cache/matugen/obsidian-matugen.css; wallpaper-post.sh (untracked)
    copies it to the vault. wallpaper-post.example.sh documents the pattern.

--- wallpaper/.local/bin/wallpaper-change.sh
+++ wallpaper/.local/bin/wallpaper-change.sh
@@ -10,3 +10,6 @@ gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER"
 
 matugen image "$WALLPAPER" -m dark -q --source-color-index 0 --type scheme-expressive
+
+POST_SCRIPT="$HOME/.local/bin/wallpaper-post.sh"
+[ -x "$POST_SCRIPT" ] && "$POST_SCRIPT" "$WALLPAPER"
---
+++ wallpaper/.local/bin/wallpaper-post.example.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+OBSIDIAN_THEME="<obsidian-vault-path>/.obsidian/snippets/obsidian-matugen.css"
+cp ~/.cache/matugen/obsidian-matugen.css "$OBSIDIAN_THEME"
