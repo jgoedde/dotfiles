e6bc01ff774c8ef9261fd93a340d72b579781f35
Julian Gödde <j.goedde@pragmaticminds.de>
2026-06-26 08:59:22 +0200

    tweak prompt order and remove some sectinos for performance

--- zsh/.spaceshiprc.zsh
+++ zsh/.spaceshiprc.zsh
@@ -1,3 +1,31 @@
 # Disable async prompt. See https://github.com/spaceship-prompt/spaceship-prompt/issues/1193
 SPACESHIP_PROMPT_ASYNC=false
 SPACESHIP_USER_SHOW=always
+
+SPACESHIP_PROMPT_ORDER=(
+  time           # Time stamps section
+  user           # Username section
+  dir            # Current directory section
+  host           # Hostname section
+  git            # Git section (git_branch + git_status)
+  package        # Package version
+  node           # Node.js section
+  bun            # Bun section
+  deno           # Deno section
+  python         # Python section
+  docker         # Docker section
+  docker_compose # Docker section
+  venv           # virtualenv section
+  conda          # conda virtualenv section
+  uv             # uv section
+  dotnet         # .NET section
+  gnu_screen     # GNU Screen section
+  exec_time      # Execution time
+  async          # Async jobs indicator
+  line_sep       # Line break
+  battery        # Battery level and status
+  jobs           # Background jobs indicator
+  exit_code      # Exit code section
+  sudo           # Sudo indicator
+  char           # Prompt character
+)
