8558d98fca4036a27840be38498cdd68fb9b93af
Julian Gödde <juliangd02@gmail.com>
2026-07-18 08:10:41 +0200

    add knew alias to reopen kitty at current loc

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -5,6 +5,7 @@ alias lt="eza -a --tree --level=1 --icons=always"
 alias shutdown="systemctl poweroff"
 alias ff="fastfetch"
 alias cat="bat"
+alias knew='(setsid kitty --directory="$PWD" >/dev/null 2>&1 &); exit'
 
 # bat for --help and -h
 alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
