76a66f31c32d014a7fccbbd0331ef60761df832a
Julian Gödde <juliangd02@gmail.com>
2026-07-18 08:36:02 +0200

    rice it up

--- zsh/.config/zsh/10-path.zsh
+++ zsh/.config/zsh/10-path.zsh
@@ -1,25 +1,43 @@
 export PATH=$PATH:~/.local/bin/
 
+#-----------------------------------
+# NVM
+#-----------------------------------
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+#-----------------------------------
+# pnpm
+#-----------------------------------
 export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
 
+#-----------------------------------
+# pyenv
+#-----------------------------------
 export PYENV_ROOT="$HOME/.pyenv"
 if [[ -d $PYENV_ROOT/bin ]]; then
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init - zsh)"
 fi
 
-[ -d "$HOME/.local/bin/zoxide" ] && eval "$(zoxide init zsh --cmd cd)"
+#-----------------------------------
+# zoxide
+#-----------------------------------
+command -v zoxide >/dev/null && eval "$(zoxide init zsh --cmd cd)"
 
+#-----------------------------------
+# brew
+#-----------------------------------
 if [[ -d /home/linuxbrew ]]; then
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
 fi
 
+#-----------------------------------
+# deno
+#-----------------------------------
 [ -d $HOME/.deno ] && source "$HOME/.deno/env"
