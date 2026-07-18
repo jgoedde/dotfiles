2719176df2754573f80712889c2c1737211afdef
Julian Gödde <129423545+jgoedde@users.noreply.github.com>
2026-07-18 15:17:21 +0200

    Update wallpaper-change.sh

--- wallpaper/.local/bin/wallpaper-change.sh
+++ wallpaper/.local/bin/wallpaper-change.sh
@@ -9,7 +9,7 @@ WALLPAPER=$(find "$WALLS_DIR" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -i
 gsettings set org.gnome.desktop.background picture-uri "file://$WALLPAPER"
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER"
 
-matugen image "$WALLPAPER" -m dark -q --source-color-index 0 --type scheme-expressive
+matugen image "$WALLPAPER" -m dark -q --source-color-index 0
 
 POST_SCRIPT="$HOME/.local/bin/wallpaper-post.sh"
 [ -x "$POST_SCRIPT" ] && "$POST_SCRIPT" "$WALLPAPER"
