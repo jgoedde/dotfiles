33def89f895d4d754c20f7d26503e8b6109bccc6
Julian Gödde <juliangd02@gmail.com>
2026-08-02 08:45:12 +0200

    use matugen colors for fzf

--- zsh/.config/zsh/30-customization.zsh
+++ zsh/.config/zsh/30-customization.zsh
@@ -1,9 +1,16 @@
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+#-----------------------------------
+# fzf
+#-----------------------------------
+export FZF_DEFAULT_OPTS_FILE="$HOME/.config/fzf/fzf-colors.conf"
 
 export FZF_CTRL_T_OPTS="
   --walker-skip .git,node_modules,target
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
-  
-export _ZO_DOCTOR=0
 
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+#-----------------------------------
+# zoxide
+#-----------------------------------
+export _ZO_DOCTOR=0
