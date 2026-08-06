c0257536650f184d3ae9de1945599eded060d6e5
Julian Gödde <j.goedde@pragmaticminds.de>
2026-08-06 08:06:15 +0200

    Add `dots` alias to cd into `~/dotfiles`

--- zsh/.config/zsh/20-aliases.zsh
+++ zsh/.config/zsh/20-aliases.zsh
@@ -15,3 +15,5 @@ alias knew='(setsid kitty --directory="$PWD" >/dev/null 2>&1 &); exit'
 # bat for --help and -h
 alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
 alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
+alias dots="cd ~/dotfiles"
