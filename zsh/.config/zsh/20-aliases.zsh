0b75f859f1ab26ff59fba33d1209b0a19da5b57b
Julian Gödde <juliangd02@gmail.com>
2026-09-06 08:59:11 +0200

    feat(zsh): Add some aliases from https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/common-aliases/README.md

--- zsh/.config/zsh/20-aliases.zsh
+++ zsh/.config/zsh/20-aliases.zsh
@@ -12,6 +12,12 @@ alias ff="fastfetch"
 alias cat="bat"
 alias knew='(setsid kitty --directory="$PWD" >/dev/null 2>&1 &); exit'
 
+# Common aliases: Hand-picked from https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/common-aliases/README.md
+alias rm="rm -i"
+alias cp="cp -i"
+alias mv="mv -i"
+alias zshrc="${=EDITOR} ~/.zshrc"
+
 # bat for --help and -h
 alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
 alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
