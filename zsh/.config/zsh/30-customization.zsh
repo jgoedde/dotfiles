aa0a7bc3a33202c7bbc459c7d2ec64625eb850c7
Julian Gödde <j.goedde@pragmaticminds.de>
2026-09-25 12:07:03 +0200

    feat(zsh): Tweak less settings
    Only show pager when useful

--- zsh/.config/zsh/30-customization.zsh
+++ zsh/.config/zsh/30-customization.zsh
@@ -7,3 +7,9 @@ source "$HOME/.config/fzf/fzf-config"
 # zoxide
 #-----------------------------------
 export _ZO_DOCTOR=0
+
+# Get pager only when useful
+# -F quits immediately if the output fits on one screen.
+# -R keeps colors.
+# -X leaves the output visible after quitting.
+export LESS=-FRX
